c4f216fc3005dfab0204e10a169145fce282ec3f
skrevolve <sukyu0919@naver.com>
2023-06-12 11:46:15 +0900

    delete

--- out/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./src/app");
-const blog_controller_1 = require("./src/controllers/blog.controller");
-const app = new app_1.App([
-    new blog_controller_1.BlogController(),
-], 8080);
-app.listen();
-//# sourceMappingURL=main.js.map
